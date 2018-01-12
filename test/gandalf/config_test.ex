e6a152166f36553f5213fb908c28549608cf4a74
Ivan Santos <pragmaticivan@gmail.com>
2018-01-12 06:31:49 -0800

    Adds more tests

---
+++ test/gandalf/config_test.ex
@@ -0,0 +1,92 @@
+defmodule Gandalf.ConfigTest do
+  use ExUnit.Case
+  alias Gandalf.Config
+
+  test ".repo" do
+    assert Gandalf.Repo == Config.repo()
+  end
+
+  test ".scopes" do
+    assert ~w(read write session) == Config.scopes()
+  end
+
+  test ".app_scopes" do
+    assert "read,write,session" == Config.app_scopes()
+  end
+
+  test ".grant_types" do
+    grant_types = %{
+      authorization_code: Gandalf.GrantType.AuthorizationCode,
+      client_credentials: Gandalf.GrantType.ClientCredentials,
+      password: Gandalf.GrantType.Password,
+      refresh_token: Gandalf.GrantType.RefreshToken
+    }
+    assert grant_types == Config.grant_types()
+  end
+
+  test ".auth_strategies" do
+    auth_strategies = %{
+      headers: %{
+        "authorization" => [
+          {~r/Basic ([a-zA-Z\-_\+=]+)/, Gandalf.Authentication.Basic},
+          {~r/Bearer ([a-zA-Z\-_\+=]+)/, Gandalf.Authentication.Bearer},
+        ],
+        "x-api-token" => [
+          {~r/([a-zA-Z\-_\+=]+)/, Gandalf.Authentication.Bearer}
+        ]
+      },
+      query_params: %{
+        "access_token" => Gandalf.Authentication.Bearer
+      },
+      sessions: %{
+        "session_token" => Gandalf.Authentication.Session
+      }
+    }
+    assert auth_strategies == Config.auth_strategies()
+  end
+
+  test ".header_auth" do
+    headers = %{
+      "authorization" => [
+        {~r/Basic ([a-zA-Z\-_\+=]+)/, Gandalf.Authentication.Basic},
+        {~r/Bearer ([a-zA-Z\-_\+=]+)/, Gandalf.Authentication.Bearer},
+      ],
+      "x-api-token" => [
+        {~r/([a-zA-Z\-_\+=]+)/, Gandalf.Authentication.Bearer}
+      ]
+    }
+    assert headers == Config.header_auth()
+  end
+
+  test ".query_params_auth" do
+    query_params = %{
+      "access_token" => Gandalf.Authentication.Bearer
+    }
+    assert query_params == Config.query_params_auth()
+  end
+
+  test ".session_auth" do
+    session_auth = %{
+      "session_token" => Gandalf.Authentication.Session
+    }
+    assert session_auth == Config.session_auth()
+  end
+
+  test ".expires_in" do
+    expires_in = %{
+      access_token: 3600,
+      refresh_token: 24 * 3600,
+      authorization_code: 300,
+      session_token: 30 * 24 * 3600
+    }
+    assert expires_in == Config.expires_in()
+  end
+
+  test ".renderer" do
+    assert Gandalf.Renderer.RestApi == Config.renderer()
+  end
+
+  test ".app_authorization" do
+    assert Gandalf.Stub.AppAuthorization == Config.app_authorization()
+  end
+end
---
+++ test/gandalf/oauth2_test.exs
@@ -0,0 +1,57 @@
+defmodule Gandalf.OAuth2Test do
+  use ExUnit.Case
+  use Gandalf.RepoBase
+  import Gandalf.Factory
+  alias Gandalf.OAuth2
+  alias Gandalf.Error.SuspiciousActivity, as: SuspiciousActivityError
+
+  @redirect_uri "https://xyz.com/rd"
+  @scopes "read"
+
+  test ".authorize raise when strategy not exist" do
+    params = %{"grant_type" => "urn"}
+    assert_raise SuspiciousActivityError, fn -> OAuth2.authorize(params) end
+  end
+
+  test ".grant_app_authorization delegates with right params" do
+    resource_owner = insert(:user)
+    client_owner = insert(:user)
+    client = insert(:client, user_id: client_owner.id,
+      redirect_uri: @redirect_uri)
+    params = %{"client_id" => client.id, "redirect_uri" => @redirect_uri,
+      "scope" => @scopes}
+
+    OAuth2.grant_app_authorization(resource_owner, params)
+    assert_receive :ok
+  end
+
+  test ".authorize_app delegates with right params" do
+    resource_owner = insert(:user)
+    client_owner = insert(:user)
+    client = insert(:client, user_id: client_owner.id,
+      redirect_uri: @redirect_uri)
+    params = %{"client_id" => client.id, "redirect_uri" => @redirect_uri,
+      "scope" => @scopes}
+
+    OAuth2.authorize_app(resource_owner, params)
+    assert_receive :ok
+  end
+
+  test ".revoke_app_authorization delegates with right params" do
+    resource_owner = insert(:user)
+    client_owner = insert(:user)
+    client = insert(:client, user_id: client_owner.id,
+                    redirect_uri: @redirect_uri)
+    app = insert(:app, user_id: resource_owner.id, client_id: client.id,
+                 scope: @scopes)
+    insert(:access_token, user_id: resource_owner.id, details: %{
+      client_id: client.id
+    })
+    insert(:refresh_token, user_id: resource_owner.id, details: %{
+      client_id: client.id
+    })
+
+    OAuth2.revoke_app_authorization(resource_owner, %{"id" => app.id})
+    assert_receive :ok
+  end
+end
