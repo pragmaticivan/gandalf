d6f79489104f3b3544247856bb93679f47ec9e0d
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2018-04-16 21:16:41 -0700

    Defines models from config with fallback for Gandalf default models

--- lib/gandalf/repo_base.ex
+++ lib/gandalf/repo_base.ex
@@ -7,10 +7,10 @@ defmodule Gandalf.RepoBase do
     quote do
       alias Gandalf.Model.{User, Token, Client, App}
 
-      @resource_owner User
-      @token_store Token
-      @client Client
-      @app App
+      @resource_owner Application.get_env(:gandalf, :resource_owner, User)
+      @token_store Application.get_env(:gandalf, :token_store, Token)
+      @client Application.get_env(:gandalf, :client, Client)
+      @app Application.get_env(:gandalf, :app, App)
     end
   end
 end
